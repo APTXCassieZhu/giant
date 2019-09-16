4d1a537ac936c1cfba5afde76b8a801c1638465e
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-16 17:35:44 +0800

    try to use component

--- assetstore/src/components/HelloWorld.vue
+++ assetstore/src/components/HelloWorld.vue
@@ -1,113 +1,321 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+    <div id="top-nav" class="topnav">
+        <div class="topnav-box">
+            <div class="topnav-box-menu">
+                <Icon type="ios-menu" size="48"></Icon>
+            </div>
+            <div class="topnav-box-logo">
+                <img src="../assets/logo.png" alt="logo">
+            </div>
+
+            <!--不知道怎么清除之前div css-->
+            <div class="topnav-box-image">
+                <img src="../assets/logo.png" alt="logo">
+            </div>
+
+            <div class="topnav-box-link">
+                <div class="topnav-box-link-a">
+                    <router-link to="/" class="active">首页</router-link>
+                </div>
+                <div class="topnav-box-link-a">
+                    <Dropdown>
+                        <a href="javascript:void(0)">资源
+                            <Icon type="ios-arrow-down"></Icon>
+                        </a>
+                        <DropdownMenu slot="list" class="topnav-dropdown">
+                            <ul><DropdownItem>2D</DropdownItem></ul>
+                            <ul><DropdownItem>3D</DropdownItem></ul>
+                            <ul><DropdownItem>贴图与材质</DropdownItem></ul>
+                            <ul><DropdownItem>模板</DropdownItem></ul>
+                            <ul><DropdownItem>3D动画</DropdownItem></ul>
+                            <ul><DropdownItem>GUI界面</DropdownItem></ul>
+                            <ul><DropdownItem>特效</DropdownItem></ul>
+                            <ul><DropdownItem>音效</DropdownItem></ul>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+                <div class="topnav-box-link-a">
+                    <Dropdown>
+                        <a href="javascript:void(0)">工具<Icon type="ios-arrow-down"></Icon></a>
+                        <DropdownMenu slot="list" class="topnav-dropdown">
+                            <ul><DropdownItem>可视化脚本</DropdownItem></ul>
+                            <ul><DropdownItem>地形</DropdownItem></ul>
+                            <ul><DropdownItem>动画编辑器</DropdownItem></ul>
+                            <ul><DropdownItem>粒子和效果</DropdownItem></ul>
+                            <ul><DropdownItem>其他工具</DropdownItem></ul>
+                            <ul><DropdownItem>AI</DropdownItem></ul>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+                <div class="topnav-box-link-a">
+                    <a onlick="gosoftware()">软件</a>
+                </div>
+                <div class="topnav-box-link-a">
+                    <a onlick="goknowledge()">知识</a>
+                </div>
+                <div class="topnav-box-link-a">
+                    <a onlick="goQ&A()">问答</a>
+                </div>
+            </div>
+            
+            <div class="topnav-box-search">
+                <!--提交搜索内容-->
+                <form action="" id="keyword-search-form">
+                    <Input id="keyword-search" type=search placeholder="输入关键词"/>
+                    <button type="submit"><Icon type="ios-search" size="36"></Icon></button>
+                </form>
+            </div>
+
+            <div class="topnav-box-user">
+                <Icon type="ios-contact" size="48" onclick="gologin()"/>
+                <span class="topnav-tooltip">登录</span>
+            </div>
+
+            <div class="topnav-box-user-login">
+                <Tooltip content="登录" placement="top" style="position:fixed; z-index:1000;">
+                    <Icon type="ios-contact" size="48"/>
+                </Tooltip>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+    
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.topnav {
+    overflow: hidden;
+    width: 100%;
+    height: 80px;
+    margin: 0;
+    padding: 0;
+    border-color: grey;
+    background-color: aliceblue;
+    border-width: 1px;  
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.topnav-box{
+    overflow: hidden;
+    position: fixed;
+    margin-top: 15px;
+    width: 100%;
+    height: 80px;
+    /*column-count: 4;*/
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+/*only show when the screen is small*/
+.topnav-box-menu, .topnav-box-user-login, .topnav-box-image {
+    display: none;
 }
-a {
-  color: #42b983;
+
+.topnav-box-logo{
+    position: relative;
+    width: 5%;
+    left: 4%;
+    float:left;
 }
+
+.topnav-box-logo img {
+    width: 50px;
+    height: 50px;
+    /*border-radius: 50%;*/
+    border-color: black;
+    border-width: 0px;  
+    border-style: solid;
+    align-content: center;
+}
+
+.topnav-box-link {
+    position: relative;
+    float:left;
+    left: 5%;
+    top: 10px;
+    width: 60%;
+    height: 80px;
+    font-size: 18px;
+    color: black;
+}
+
+.topnav-box-link-a{
+    position: relative;
+    text-decoration: none;
+    width: 12%;
+    float: left;
+    padding-right: 2%;
+    color: black;
+    text-align:center;    
+}
+
+.topnav-box-search{
+    position: relative;
+    width: 25%;
+    /*display: inline-block;*/
+    float:left;
+    font-size: 15px;
+}
+
+.topnav-box-search input[type=search]{
+    float: left;
+    text-align: left;
+    width:80%;
+    margin: 0;
+    padding: 14px;
+}
+.topnav-box-search button{
+    background-color: aliceblue;
+    float: left;
+    width: 54px;
+    height: 54px;
+    cursor: pointer;
+}
+
+.topnav-box-search button:hover{
+    background-color: #6495ED;
+    color: white;
+    border-width: 3px;
+}
+
+.topnav-box-user{
+    position: relative;
+    width: 7%;
+    float:right;
+    align-content: center;
+}
+
+
+
+.topnav-box-link-a:hover, .topnav-box-user:hover{
+    color: #6495ED;
+    border-top: 5px solid #6495ED;
+    cursor: pointer;
+}
+
+.topnav-box-link-a a.active{
+    color: #BA55D3;
+}
+
+/*text-decoration: none;
+    width: 12%;
+    float: left;
+    padding-right: 2%;
+    color: black;
+        
+    border-right-color: grey;
+    border-right-width: 2px;*/
+
+.topnav-dropdown{
+    font-size: 18px;
+    position: fixed;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    background-color: rgba(255, 255, 255, 0.988);
+    z-index: 100;
+}
+
+.topnav-dropdown ul{
+    padding: 8px;
+}
+
+.topnav-dropdown ul:hover{
+    background-color: black;
+    color: white;
+}
+
+/*.topnav-dropdown-trigger:hover .topnav-dropdown-test{
+    display: block;
+}
+.topnav-dropdown-test{
+    display: none;
+    font-size: 18px;
+    position: fixed;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    background-color: rgba(255, 255, 255, 0.988);
+    z-index: 100;
+}
+
+.topnav-dropdown-test ul{
+    padding: 8px;
+}
+
+.topnav-dropdown-test ul:hover{
+    background-color: black;
+    color: white;
+}*/
+
+@media only screen and (max-width: 720px) {
+    .topnav-box-image{
+        display: block;
+        position: relative;
+        width: 80%;
+        height: 55px;
+        float: left;
+    }
+
+    .topnav-box-image img {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border-color: black;
+        border-width: 0px;  
+        border-style: solid;
+        left: 50%;
+        right: 50%;
+        position: relative;
+        float: center;
+    }
+
+    .topnav-box-user-login {
+        display: block;
+        width:10%;
+        margin-right: auto;
+        margin-left: 0px;
+        position: relative;
+        float: left;
+    }
+
+    .topnav-box-menu {
+        width:10%;
+        display: block;
+        position: relative;
+        margin-left: auto;
+        margin-right: 0px;
+        float: left;
+    }
+
+    .topnav-box-link, .topnav-box-search, .topnav-box-user, .topnav-box-logo{
+        display: none;
+    }
+}
+
+@media only screen and (max-width: 800px) {
+    .topnav-box-logo {
+        left: 2%;
+    }
+
+    .topnav-box-link {
+        left: 10%;
+    }
+    
+}
+
+@media only screen and (max-width: 980px) {
+    .topnav-box-logo {
+        left: 3%;
+    }
+}
+
+@media only screen and (max-width: 1080px) {
+    .topnav-box-search input[type=search] {
+        width:70%;
+    }
+
+    .topnav-box-user span{
+        display: none;
+    }
+}
+
 </style>
+
