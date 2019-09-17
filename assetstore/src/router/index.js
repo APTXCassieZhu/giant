d2866e5f98e0718d161caf19bc543bb2d399933e
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-17 17:52:32 +0800

    / redirect to /home

--- assetstore/src/router/index.js
+++ assetstore/src/router/index.js
@@ -10,6 +10,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      redirect: '/home'
+    },
+    {
       path: '/home',
       name: 'Top Navigation',
       component: TopNav
