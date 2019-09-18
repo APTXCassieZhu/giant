64d3407af50293ac68bcea9fdce6cefeb11dd63c
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-18 21:43:53 +0800

    adjust

--- assetstore/src/components/Search.vue
+++ assetstore/src/components/Search.vue
@@ -1,7 +1,12 @@
 <template>
     <Card>
-        <p slot="title" style="color: blue">热门搜索</p>
-        <Icon type="ios-repeat"></Icon>换一批
+        <span slot="title" style="color: blue">热门搜索
+        <Icon type="ios-repeat"></Icon>换一批</span>
+        <Tag color="blue">three.js</Tag>
+        <Tag color="blue">JiaFu</Tag>
+        <Tag color="blue">Candy</Tag>
+        <Tag color="blue">仙侠</Tag>
+        <Tag color="blue">lalalala</Tag>
         <Divider />
 
     </Card>
