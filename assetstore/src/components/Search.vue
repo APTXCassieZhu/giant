d77ee198b08e3d81f2c8bd453ad1dd9f529d291b
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-18 18:20:48 +0800

    try another component

---
+++ assetstore/src/components/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <Card>
+        <p slot="title" style="color: blue">热门搜索</p>
+        <Icon type="ios-repeat"></Icon>换一批
+        <Divider />
+        
+    </Card>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
