a37c72f8201b9ecc9d77dc898d4fe2b6685d68f8
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-23 16:09:26 +0800

    search 推荐格式调整

--- assetstore/src/components/Search.vue
+++ assetstore/src/components/Search.vue
@@ -1,11 +1,25 @@
 <template>
-    <Card>
+    <Card id="card">
         <span slot="title" align="left" style="color: blue">热门搜索</span>
-        <span slot="title" align="center" ></span>
-        <span slot="title" align="right"><Icon type="ios-repeat" @click="change()"></Icon>换一批</span>
+        <span slot="title" align="center" >&emsp;&emsp;&emsp;&emsp;&emsp;</span>
+        <span slot="title" align="center" >&emsp;&emsp;&emsp;&emsp;&emsp;</span>
+        <span slot="title" align="right" style="color: blue; cursor: pointer;" @click="change()"><Icon size="20" type="ios-repeat"></Icon>换一批</span>
+        <span slot="title" align="center" >&emsp;&emsp;&emsp;&emsp;&emsp;</span>
+        <span slot="title" align="center" >&emsp;&emsp;&emsp;&emsp;&emsp;</span>
+        <span slot="title" align="center" class="close">
+            <Icon size="20" type="ios-close-circle-outline" @click="close()"></Icon>
+        </span>
         <span v-for="(item,index) in tagList" :key="index">
             <Tag color="blue">{{item}}</Tag>
+            <span>&emsp;</span>
         </span>
+        <br>
+        <br>
+        <ul>
+            <Icon size="20" type="ios-time-outline"></Icon><Tag color="orange">历史搜索1</Tag>
+            <Icon size="20" type="ios-time-outline"></Icon><Tag color="orange">历史搜索2</Tag>
+        </ul>
+        <ul><Icon size="20" type="ios-time-outline"></Icon><Tag color="orange">历史搜索3</Tag></ul>
         <Divider />
 
     </Card>
@@ -13,14 +27,13 @@
 </template>
 
 <script>
-import * as math from "mathjs"
 export default {
     name:"Search",
     data() {
         return {
             data: ['three.js', 'jiaFu', 'juju', 'ruirui', 'candy', '仙侠', 
             'lala', 'detective', '凑数', '饿了', '好饿', '饿的不行了', '再编几个', 
-            '想编几个编几个','conan', 'sos'],
+            '想编几个编几个','conan', 'sos'], // total: 16
             tagList: [],     //存放每次点击换一批放出来的5个对象
             arr: [],         //存放从原来的data array抽选的index，确保不重复
             num: ''          //随机index
@@ -40,7 +53,7 @@ export default {
             this.arr = []
             this.tagList = []
             while(this.arr.length < 5) {
-                let num = parseInt(Math.random()*15)
+                let num = parseInt(Math.random()*16)
                 if(this.arr.indexOf(num) == -1) {
                     console.log("1111111")
                     this.arr.push(num)
@@ -48,6 +61,9 @@ export default {
                     this.tagList.push(this.data[this.num])
                 }
             }
+        },
+        close() {
+            document.getElementById("card").style.display="none";
         }
     }
 }
@@ -56,5 +72,12 @@ export default {
 <style scoped>
 .Card {
     display: none;
+}
+.close {
+    cursor: pointer;
+}
+
+.close:hover {
+    color: red;
 }
 </style>
