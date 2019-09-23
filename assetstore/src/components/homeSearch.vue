9e8f4c2ab8435bd9df52e45418d4a88e0c361534
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-23 17:22:28 +0800

    搜索样式调整2.0版
    1.去掉“换一批”
    2.每次focus自动刷新一批

---
+++ assetstore/src/components/homeSearch.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="home-search-wrapper">
+        <Form ref="searchForm" :model="searchForm" :rules="searchRule">
+            <FormItem prop="content">
+                <div class="home-search-container">
+                    <Input id="search" size="large" type="text" class="search-input" 
+                    @focus.native.capture="changeAdvise()" @blur.native.capture="hideAdvise()"
+                    placeholder="支持输入资源、用户、文章关键字" v-model="searchForm.content"/></Input>
+                    <Button type="primary" class="search-button">
+                        <Icon type="ios-search" size="30"></Icon>
+                    </Button>
+                    <Card style="width:80%; top:40px;">
+                        <span style="color: blue; font-size: 19px;">热门搜索</span>
+                        <span>&emsp;</span>
+                        <span v-for="(item,index) in tagList" :key="index">
+                            <Tag color="blue">{{item}}</Tag>
+                            <span>&emsp;</span>
+                        </span>
+                        <Divider/>
+                        <ul><Icon size="20" type="ios-time-outline"></Icon><Tag color="orange">历史搜索1</Tag></ul>
+                        <ul><Icon size="20" type="ios-time-outline"></Icon><Tag color="orange">历史搜索3</Tag></ul>
+                    </Card>
+                </div>
+            </FormItem>
+        </Form>
+    </div>
+</template>
+
+<script>
+function contain(str, charset) {
+    var i;
+    for(i=0; i<charset.length; i++) {
+        if(str.indexOf(charset.charAt(i))>=0) {
+            return true;
+        }
+    }
+    return false;
+}
+export default {
+    name:"HomeSearch",
+    data() {
+        const validateContent = (rule, value, callback) => {
+            if(contain(value, "^[!@#$%&*()-+=.~`]_{}?/<>,")) {
+                callback(new Error("含有非法字符"))
+            } else {
+                callback();
+            }
+        }
+        return {
+            data: ['three.js', 'jiaFu', 'juju', 'ruirui', 'candy', '仙侠', 
+            'lala', 'detective', '凑数', '饿了', '好饿', '饿的不行了', '再编几个', 
+            '想编几个编几个','conan', 'sos'], // total: 16
+            tagList: [],     //存放每次点击换一批放出来的5个对象
+            arr: [],         //存放从原来的data array抽选的index，确保不重复
+            num: '',         //随机index
+            searchForm: {content:""},
+            searchRule: {
+                content: [{required: true, trigger:'blur', validator: validateContent}
+                ]
+            }
+        }
+    },
+    // 页面加载时就自动生成推荐内容
+    mounted() {
+        for(var i = 0; i < 5; i++) {
+            this.tagList.push(this.data[i])
+        }
+    },
+    methods: {
+        searchSubmit() {
+            console.log(this.searchForm);
+            axios.post('/search',{searchForm: this.search}).then((response)=>{
+                //alert("提交成功^_^，刚刚提交内容是：" + response.body.search)
+            }, (response)=>{
+                //alert("出错啦QAQ")
+            })
+        },
+        // 每次点击换一批，更换推荐内容
+        changeAdvise() {
+            document.getElementById("content").style.display="block"
+            // 清空之前的数据
+            this.num = ''
+            this.arr = []
+            this.tagList = []
+            while(this.arr.length < 5) {
+                let num = parseInt(Math.random()*16)
+                if(this.arr.indexOf(num) == -1) {
+                    this.arr.push(num)
+                    this.num = num
+                    this.tagList.push(this.data[this.num])
+                }
+            }
+        },
+        hideAdvise() {
+            document.getElementById("content").style.display='none';
+        }
+    }
+}
+</script>
+
+<style scoped>
+.home-search-wrapper{
+    position: absolute;
+    border-color:#6495ED;
+    border-width:5px;
+    height: 400px;
+    top: 80px;
+    float: none;
+    width: 100%;
+    z-index: -10;
+    background-image: url("../assets/blue.jpg")
+}
+
+.home-search-wrapper img{
+    height: 400px;
+    float: center;
+    width: 80%;
+    z-index: 0;
+}
+
+.home-search-container {
+    position: absolute;
+    float: right;
+    left: 20%;
+    top: 180px;
+    width: 60%;
+    height: 400px;
+    z-index: 0;
+}
+
+.search-input{
+    text-align: left;
+    float: left;
+    width: 80%;
+    height: 48px;
+    z-index: 0;
+}
+
+.search-button{
+    background-color: #6495ED;
+    float: left;
+    width: 20%;
+    cursor: pointer;
+    height: 36px;
+    z-index: 0;
+}
+
+.search-button:hover{
+    background-color: #BA55D3;
+    border-color: #BA55D3;
+    color: white;
+    border-width: 3px;
+    z-index: 0;
+}
+
+.Card {
+    display: none;
+    z-index: 10;
+}
+.close {
+    cursor: pointer;
+}
+
+.close:hover {
+    color: red;
+}
+</style>
--- assetstore/src/pages/home.vue
+++ assetstore/src/pages/home.vue
@@ -1,132 +1,24 @@
 <template>
 <div>
     <TopNavigation></TopNavigation>
-    <div class="home-search-wrapper">
-        <Form ref="searchForm" :model="searchForm" :rules="searchRule">
-            <FormItem prop="content">
-                <div class="home-search-container">
-                    <Input id="search" size="large" type="text" class="search-input" 
-                    @focus.native.capture="showAdvise()"
-                    placeholder="支持输入资源、用户、文章关键字" v-model="searchForm.content"/></Input>
-                    <Button type="primary" class="search-button">
-                        <Icon type="ios-search" size="30"></Icon>
-                    </Button>
-                    <div>
-                        <Search id="content" style="width:80%; top:40px;"></Search>
-                    </div>
-                </div>
-            </FormItem>
-        </Form>
-    </div>
+    <HomeSearch></HomeSearch>
 </div>
 </template>
 
 <script>
 import TopNavigation from '../components/TopNav.vue'
-import Search from '../components/Search.vue'
-
+import HomeSearch from '../components/homeSearch.vue'
 /*var searchContent = document.getElementById("search")
 addEventListener(searchContent, "focus", function() {
 
 })*/
-function contain(str, charset) {
-    var i;
-    for(i=0; i<charset.length; i++) {
-        if(str.indexOf(charset.charAt(i))>=0) {
-            return true;
-        }
-    }
-    return false;
-}
 
 export default {
     name:"Home",
-    components: {TopNavigation, Search},
-    data() {
-        const validateContent = (rule, value, callback) => {
-            if(contain(value, "^[!@#$%&*()-+=.~`]_{}?/<>,")) {
-                callback(new Error("含有非法字符"))
-            } else {
-                callback();
-            }
-        }
-        return {
-            searchForm: {content:""},
-            searchRule: {
-                content: [{required: true, trigger:'blur', validator: validateContent}
-                ]
-            }
-        }
-        
-    },
-    mounted() {
-        document.getElementById("content").style.display='none';
-    },
-    methods: {
-        searchSubmit() {
-            console.log(this.searchForm);
-            axios.post('/search',{searchForm: this.search}).then((response)=>{
-                //alert("提交成功^_^，刚刚提交内容是：" + response.body.search)
-            }, (response)=>{
-                //alert("出错啦QAQ")
-            })
-        },
-        showAdvise() {
-            document.getElementById("content").style.display='block';
-        },
-        hideAdvise() {
-            document.getElementById("content").style.display='none';
-        }
-    }
+    components: {TopNavigation,HomeSearch},
 }
 </script>
 
 <style scoped>
-.home-search-wrapper{
-    position: absolute;
-    border-color:#6495ED;
-    border-width:5px;
-    height: 400px;
-    top: 80px;
-    float: none;
-    width: 100%;
-    background-image: url("../assets/blue.jpg")
-}
 
-.home-search-wrapper img{
-    height: 400px;
-    float: center;
-    width: 80%;
-}
-
-.home-search-container {
-    position: absolute;
-    float: right;
-    left: 20%;
-    top: 180px;
-    width: 60%;
-    height: 400px;
-}
-
-.search-input{
-    text-align: left;
-    float: left;
-    width: 80%;
-    height: 48px;
-}
-
-.search-button{
-    background-color: #6495ED;
-    float: left;
-    width: 20%;
-    cursor: pointer;
-    height: 36px;
-}
-
-.search-button:hover{
-    background-color: #BA55D3;
-    border-color: #BA55D3;
-    color: white;
-    border-width: 3px;
-}
 </style>
