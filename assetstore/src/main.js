a4febdf67a93380be93a22afa54545e9fca8c1c0
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-24 20:03:12 +0800

    404

--- assetstore/src/main.js
+++ assetstore/src/main.js
@@ -5,12 +5,14 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import axios from 'axios'
+import Vuex from 'vuex'
 import VueResource from 'vue-resource'
 //import VueCookies from 'vue-cookies'
 import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
-
+///Vue.prototype.$http = axios
+Vue.use(Vuex)
 axios.defaults.baseURL='http://localhost:8080'
 global.axios=axios
 axios.defaults.headers.post['Content-Type'] = 'application/json'
