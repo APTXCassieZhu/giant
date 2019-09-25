d8b69c966d1493c437cb36d9e7804bf28121ac1b
APTXCassieZhu <ruiqi.zhu@stonybrook.edu>
2019-09-25 11:17:04 +0800

    try use nodejs handle post request

--- assetstore/server/app.js
+++ assetstore/server/app.js
@@ -1,13 +1,22 @@
 var express = require('express')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 const app = express()
 
+var users = require('./routers/users')
 var checklogin = require('./routers/checklogin')
+
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded( {extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/checklogin', checklogin)
+app.use('/users', users)
 
 app.get('/',function(req, res){
     res.send("success")
 })
 
-app.listen(port,'0.0.0.0', () => {
+app.listen(3000,'0.0.0.0', () => {
     return console.log(`App listening on port ${port}!`);
 })
---
+++ assetstore/server/routers/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+const path = require('path');
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+router.post('/login', jsonParser, function(req,res){
+    console.log("back-end account: "+req.body.account)
+    console.log("pwd: " +req.body.password)
+    res.cookie('session', { current_user: req.body.account });
+    res.json({'status':"OK"});
+})
+//export this router to use in our index.js
+module.exports = router;
