0a6a33e3b9ee73063f51f3169b224664113cb41e
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-09-25 14:05:37 +0800

    后端测试成功 404 成功解决

--- assetstore/server/app.js
+++ assetstore/server/app.js
@@ -2,14 +2,14 @@ var express = require('express')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 const app = express()
-
+const port = 3000
 var users = require('./routers/users')
 var checklogin = require('./routers/checklogin')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( {extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+//app.use(express.static(path.join(__dirname, 'public')))
 app.use('/checklogin', checklogin)
 app.use('/users', users)
 
@@ -17,6 +17,6 @@ app.get('/',function(req, res){
     res.send("success")
 })
 
-app.listen(3000,'0.0.0.0', () => {
+app.listen(port,'0.0.0.0', () => {
     return console.log(`App listening on port ${port}!`);
 })
