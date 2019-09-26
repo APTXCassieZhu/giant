6c040b765334e1338cad667021ba1ac3b835cb86
unknown <zhuruiqi@ztgame.com>
2019-09-26 11:52:52 +0800

    fix bug of router beforeeach

--- assetstore/src/main.js
+++ assetstore/src/main.js
@@ -24,6 +24,7 @@ Vue.use(VueResource)
 const ADD_COUNT = 'ADD_COUNT'; // 用常量代替事件类型，使得代码更清晰const REMOVE_COUNT = 'REMOVE_COUNT';//注册状态管理全局参数var store = new Vuex.Store({  state:{    token:'',    userID:'',  },  mutations: {    //写法与getters相类似    //组件想要对于vuex 中的数据进行的处理    //组件中采用this.$store.commit('方法名') 的方式调用，实现充分解耦    //内部操作必须在此刻完成(同步)    [ADD_COUNT] (state, token) { // 第一个参数为 state 用于变更状态 登录      sessionStorage.setItem("token", token);      state.token = token;    },    [REMOVE_COUNT] (state, token) { // 退出登录       sessionStorage.removeItem("token", token);       state.token = token;    },  }});
 const REMOVE_COUNT = 'REMOVE_COUNT';
 const REMEM_COUNT = 'REMEM_COUNT';
+const localstorage = require('./localstorage')
 // 注册状态管理全局参数
 var store = new Vuex.Store({
   state:{
@@ -41,7 +42,6 @@ var store = new Vuex.Store({
       if(state.single){
         // 用户勾选记住登录状态
         console.log("记住登录30天")
-        var localstorage = require('./localstorage')
         localstorage.setAge(30*24*60*60*1000).set("token", token)
       }else{
         // 用户未勾选记住登录状态
@@ -67,13 +67,14 @@ router.beforeEach((to,from,next) => {
   // loading 效果
   iView.LoadingBar.start();
   // 获取本地存储的token
-  store.state.token = sessionStorage.getItem("token");
+  store.state.token = localstorage.get("token");
   // 判断这个url是否需要登录权限
   if(to.meta.requireAuth) {
     if(store.state.token !== "" && store.state.token !== null) {
       console.log("login or not: "+store.state.token)
       next();
     }else{
+      console.log("not login")
       next({path:'/login', query:{redirect: to.fullPath}});
     }
   }else{
