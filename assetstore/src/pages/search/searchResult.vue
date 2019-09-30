d4aa728de3b684d58acdde8d0c904e5542feb25d
unknown <zhuruiqi@ztgame.com>
2019-09-30 10:33:55 +0800

    implement all buttons dropdowns

--- assetstore/src/pages/search/searchResult.vue
+++ assetstore/src/pages/search/searchResult.vue
@@ -19,6 +19,7 @@
             <strong>"{{getSearchContent}}"的搜索结果</strong>
         </div>
         <div class="button-wrapper">
+            <!--美术资源art-->
             <Dropdown placement="bottom-start" trigger="custom" :visible="artVisible" @on-clickoutside="handleArtClose()">
                 <Button id="art" style="width: 200px; font-size: 15px" href="javascript:void(0)" @click="handleArtOpen()"><strong>美术资源<Icon type="md-arrow-dropdown" size="20"/></strong></Button>
                 <DropdownMenu slot="list" style="width:200px;" >
@@ -40,9 +41,41 @@
                 </DropdownMenu>
             </Dropdown>
             <span>&emsp;&emsp;&emsp;</span>
-            <Button style="width: 200px; font-size: 15px;"><strong>研发工具<Icon type="md-arrow-dropdown" size="20"/></strong></Button>
+            <!--研发工具-->
+            <Dropdown placement="bottom-start" trigger="custom" :visible="toolVisible" @on-clickoutside="handleToolClose()">
+                <Button id="tool" style="width: 200px; font-size: 15px" href="javascript:void(0)" @click="handleToolOpen()"><strong>研发工具<Icon type="md-arrow-dropdown" size="20"/></strong></Button>
+                <DropdownMenu slot="list" style="width:200px;" >
+                    <CheckboxGroup v-model="toolGroup" @on-change="checkAllToolGroupChange()">
+                        <!--TO DO 此处数字应与后端互动拿到-->
+                        <DropdownItem><Checkbox label="???" style="font-size:15px">???</Checkbox></DropdownItem>
+                    </CheckboxGroup>
+                    <Divider style="margin:0"/>
+                    <DropdownItem><Checkbox
+                        :indeterminate="toolNotFull"
+                        :value="checkToolAll"
+                        @click.prevent.native="handleCheckToolAll()">全选</Checkbox>
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
             <span>&emsp;&emsp;&emsp;</span>
-            <Button style="width: 200px; font-size: 15px;"><strong>引擎<Icon type="md-arrow-dropdown" size="20"/></strong></Button>
+            <!--引擎engine-->
+            <Dropdown placement="bottom-start" trigger="custom" :visible="engineVisible" @on-clickoutside="handleEngineClose()">
+                <Button id="engine" style="width: 200px; font-size: 15px" href="javascript:void(0)" @click="handleEngineOpen()"><strong>引擎<Icon type="md-arrow-dropdown" size="20"/></strong></Button>
+                <DropdownMenu slot="list" style="width:200px;" >
+                    <CheckboxGroup v-model="engineGroup" @on-change="checkAllEngineGroupChange()">
+                        <!--TO DO 此处数字应与后端互动拿到-->
+                        <DropdownItem><Checkbox label="Unity" style="font-size:15px">Unity</Checkbox></DropdownItem>
+                        <DropdownItem><Checkbox label="Unreal" style="font-size:15px">Unreal</Checkbox></DropdownItem>
+                        <DropdownItem><Checkbox label="Cocos" style="font-size:15px">Cocos</Checkbox></DropdownItem>
+                    </CheckboxGroup>
+                    <Divider style="margin:0"/>
+                    <DropdownItem><Checkbox
+                        :indeterminate="engineNotFull"
+                        :value="checkEngineAll"
+                        @click.prevent.native="handleCheckEngineAll()">全选</Checkbox>
+                    </DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
         </div>
     </div>
 </template>
@@ -60,6 +93,15 @@ export default {
             artNotFull: true,
             checkArtAll: false,
             artGroup:[],        // 存放用户选取的美术资源筛选
+            toolVisible: false,
+            toolNotFull: true,
+            checkToolAll: false,
+            toolGroup:[],        // 存放用户选取的研发工具筛选
+            engineVisible: false,
+            engineNotFull: true,
+            checkEngineAll: false,
+            engineGroup:[],        // 存放用户选取的引擎筛选
+            
         }
     },
     methods:{
@@ -69,6 +111,18 @@ export default {
             },
         handleArtClose () {
             this.artVisible = false;
+        },
+        handleToolOpen () {
+            this.toolVisible = true;
+            },
+        handleToolClose () {
+            this.toolVisible = false;
+        },
+        handleEngineOpen () {
+            this.engineVisible = true;
+            },
+        handleEngineClose () {
+            this.engineVisible = false;
         },
         // handleCheckArtAll 和 checkAllArtGroupChange是为了用户能够更快速全选或取消全选
         handleCheckArtAll () {
@@ -99,10 +153,76 @@ export default {
             } else {
                 this.artNotFull = false;
                 this.checkArtAll = false;
-                // 子件里面没有一个选中则当前父button不高亮
+                // 子件里面没有一个选中则当前父button不高亮 
                 document.getElementById("art").style.active = false;
             }
-        }
+        },
+        handleCheckToolAll () {
+            if (this.toolNotFull) {
+                this.checkToolAll = false;
+            } else {
+                this.checkToolAll = !this.checkToolAll;
+            }
+            this.toolNotFull = false;
+
+            if (this.checkToolAll) {
+                // TO DO []填上所有研发工具
+                this.toolGroup = [];
+            } else {
+                this.toolGroup = [];
+            }
+        },
+        checkAllToolGroupChange (data) {
+            // data is undefined ???????????????
+            console.log(data);
+            // TO DO 6 改成 研发工具的种类总数
+            if (data.length === 6) {
+                this.toolNotFull = false;
+                this.checkToolAll = true;
+            } else if (data.length > 0) {
+                this.toolNotFull = true;
+                this.checkToolAll = false;
+                // 子件里面有一个选中则当前父button高亮
+                document.getElementById("tool").style.active = true;
+            } else {
+                this.toolNotFull = false;
+                this.checkToolAll = false;
+                // 子件里面没有一个选中则当前父button不高亮 
+                document.getElementById("tool").style.active = false;
+            }
+        },
+        handleCheckEngineAll () {
+            if (this.engineNotFull) {
+                this.checkEngineAll = false;
+            } else {
+                this.checkEngineAll = !this.checkEngineAll;
+            }
+            this.engineNotFull = false;
+
+            if (this.checkEngineAll) {
+                this.engineGroup = ['Unity', 'Unreal', 'Cocos'];
+            } else {
+                this.engineGroup = [];
+            }
+        },
+        checkAllEngineGroupChange (data) {
+            // data is undefined ???????????????
+            console.log(data);
+            if (data.length === 3) {
+                this.engineNotFull = false;
+                this.checkEngineAll = true;
+            } else if (data.length > 0) {
+                this.engineNotFull = true;
+                this.checkEngineAll = false;
+                // 子件里面有一个选中则当前父button高亮
+                document.getElementById("engine").style.active = true;
+            } else {
+                this.engineNotFull = false;
+                this.checkEngineAll = false;
+                // 子件里面没有一个选中则当前父button不高亮 
+                document.getElementById("engine").style.active = false;
+            }
+        },
     },
     computed:{
         getSearchContent(){
