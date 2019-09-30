32ba309bc45b1c7e2d2481d81ae13b85075ceee8
APTXCassieZhu <ruiqi.zhu@stonybrook.edu>
2019-09-30 16:53:56 +0800

    adjust css

--- assetstore/src/components/sourceCard.vue
+++ assetstore/src/components/sourceCard.vue
@@ -1,10 +1,22 @@
 <template>
-    <Card>
+    <Card style="width:250px">
+        <span>
+            <Icon type="ios-star" v-for="n in 4" :key="n"></Icon>
+            <Icon type="ios-star" v-if="item.rate >= 9.5"></Icon>
+            <Icon type="ios-star-half" v-else></Icon>
+        </span>
     </Card>
 </template>
 <script>
 export default {
-    
+    name: "SourceCard",
+    data() {
+        return{
+            item: {
+                rate: 10,
+            }
+        }
+    },
 }
 </script>
 <style scoped>
