f857df5c62ed3d2d0ed543ebded46c8e43c53948
夏木 <zhuruiqi2@ztgame.com>
2019-11-12 18:32:32 +0800

    1.消息中心计算时间

--- assetstore/src/pages/notice.vue
+++ assetstore/src/pages/notice.vue
@@ -115,7 +115,7 @@
                                 <div class="font-image">{{item.resource.name.charAt(0)}}</div>
                                 你关注的资源<span :class="markGreen(item.view)"> {{item.resource.name}} </span>被评论
                             </div>
-                            <div class="time-slot">刚刚</div>
+                            <div class="time-slot">{{getTime(item.updatedAt)}}</div>
                         </div>
                         <Divider/>
                     </div>
@@ -147,7 +147,6 @@
 import TopNavigation from '../components/TopNav.vue'
 import Footer from '../components/footer.vue'
 import Corner from '../components/corner.vue'
-
 export default {
     name:"Notice",
     components:{TopNavigation, Footer, Corner, },
@@ -232,7 +231,41 @@ export default {
             }else{
                 return 'mark-green'
             }
-        }
+        },
+        getTime(t){
+            var begin = new Date(t)
+            var end = new Date()
+            var dateDiff = end.getTime() - begin.getTime();
+            //时间差的毫秒数
+            var dayDiff = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
+            var leave1=dateDiff%(24*3600*1000)    //计算天数后剩余的毫秒数
+            var hours=Math.floor(leave1/(3600*1000))//计算出小时数
+            //计算相差分钟数
+            var leave2=leave1%(3600*1000)    //计算小时数后剩余的毫秒数
+            var minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
+            //计算相差秒数
+            var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+            var seconds=Math.round(leave3/1000)
+            console.log(" 相差 "+dayDiff+"天 "+hours+"小时 "+minutes+" 分钟"+seconds+" 秒")
+            console.log(dateDiff+"时间差的毫秒数",dayDiff+"计算出相差天数",leave1+"计算天数后剩余的毫秒数"
+                ,hours+"计算出小时数",minutes+"计算相差分钟数",seconds+"计算相差秒数");
+            // 1、 刚刚（10分钟内）
+            // 2、 ? 分钟以前（10分钟以上60分钟内）
+            // 3、 ? 小时以前（60分钟以上24小时内）
+            // 4、 昨天（24小时以上48小时内）
+            // 5、 ? 天前（48小时以上）
+            if(dayDiff >= 2){
+                return dayDiff+"天前"
+            }else if(dayDiff == 1){
+                return "昨天"
+            }else if(hours >= 1 && hours <= 23){
+                return hours+"小时以前"
+            }else if(minutes >= 11 && minutes <= 59){
+                return minutes+"分钟以前"
+            }else{
+                return "刚刚"
+            }
+        },
     },
 }
 </script>
